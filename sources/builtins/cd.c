6b3ce761cae2b106668ba2a6c922774d627d8745
lucocozz <lucocozz@student.42.fr>
2021-05-21 02:37:57 +0200

    add unset builtin and 'cd' only go to home

--- sources/builtins/cd.c
+++ sources/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: lucocozz <lucocozz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 15:43:34 by lucocozz          #+#    #+#             */
-/*   Updated: 2021/05/21 01:57:55 by lucocozz         ###   ########.fr       */
+/*   Updated: 2021/05/21 02:35:03 by lucocozz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ static char	*parse_path(const char *path)
 {
 	char	*new;
 
-	if (path[0] == '~')
+	if (path == NULL)
+		new = ft_getenv("HOME");
+	else if (path[0] == '~')
 		new = ft_strjoin(ft_getenv("HOME"), &path[1], "");
 	else if (path[0] == '.')
 		new = ft_strjoin(ft_getenv("PWD"), &path[1], "");
@@ -38,8 +40,8 @@ void	cd(const char *path)
 		old = ft_getenv("PWD");
 		ft_setenv("OLDPWD", old, 1);
 		ft_setenv("PWD", new, 1);
-		ft_putstr(new);
-		ft_putchar('\n');
+		if (path != NULL)
+			ft_printf("%s\n", new);
 	}
 	else
 		ft_printf("cd: no file or directory of type: %s\n", path);
--- sources/builtins/unset.c
+++ sources/builtins/unset.c
@@ -1,18 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   export.c                                           :+:      :+:    :+:   */
+/*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lucocozz <lucocozz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/29 15:44:08 by lucocozz          #+#    #+#             */
-/*   Updated: 2021/05/21 02:31:01 by lucocozz         ###   ########.fr       */
+/*   Created: 2021/05/21 02:31:27 by lucocozz          #+#    #+#             */
+/*   Updated: 2021/05/21 02:36:56 by lucocozz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	export(const char *name, const char *value)
+void	unset(const char *name)
 {
-
+	if (name)
+		ft_unsetenv(name);
+	else
+		ft_printf("unset: not enough arguments\n");
 }
