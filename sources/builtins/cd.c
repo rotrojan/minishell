55efe8b099033c34a7a842ee9f45d75ebaaba491
lucocozz <lucocozz@student.42.fr>
2021-05-24 23:48:29 +0200

    finish cd, made it working with getcwd()

--- sources/builtins/cd.c
+++ sources/builtins/cd.c
@@ -6,44 +6,28 @@
 /*   By: lucocozz <lucocozz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 15:43:34 by lucocozz          #+#    #+#             */
-/*   Updated: 2021/05/21 15:23:59 by lucocozz         ###   ########.fr       */
+/*   Updated: 2021/05/24 23:44:50 by lucocozz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-static char	*parse_path(const char *path)
-{
-	char	*new;
-
-	if (path == NULL)
-		new = ft_strdup(ft_getenv("HOME"));
-	else if (path[0] == '~')
-		new = ft_strjoin(ft_getenv("HOME"), &path[1], "");
-	else if (path[0] == '.')
-		new = ft_strjoin(ft_getenv("PWD"), &path[1], "");
-	else if (ft_strcmp(path, "-") == 0)
-		new = ft_strdup(ft_getenv("OLDPWD"));
-	else
-		new = ft_strdup(path);
-	return (new);
-}
 
 void	cd(const char *path)
 {
 	char		*new;
 	char		*old;
 
-	new = parse_path(path);
-	if (chdir(new) != -1)
+	if (chdir(path) != -1)
 	{
+		new = getcwd(NULL, 0);
+		if (new == NULL)
+			exit_shell(EXIT_FAILURE, "getcwd(): failed to get pwd.");
 		old = ft_getenv("PWD");
 		ft_setenv("OLDPWD", old, 1);
 		ft_setenv("PWD", new, 1);
-		if (path != NULL)
-			ft_printf("%s\n", new);
+		ft_printf("%s\n", new);
+		free(new);
 	}
 	else
 		ft_printf("cd: no file or directory of type: %s\n", path);
-	gc_free(new);
 }
