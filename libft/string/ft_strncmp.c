a425ac66964788417ac2a421ca56dd0bbfd6cbaf
lucocozz <lucocozz@student.42.fr>
2021-05-24 16:11:47 +0200

    strcmp

--- libft/string/ft_strncmp.c
+++ libft/string/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: lucocozz <lucocozz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 23:53:34 by lucocozz          #+#    #+#             */
-/*   Updated: 2021/05/24 15:59:40 by lucocozz         ###   ########.fr       */
+/*   Updated: 2021/05/24 16:11:15 by lucocozz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	int	i;
 
 	i = 0;
-	while (s1[i] == s2[i] && s1[i] && s2[i] && i < n - 1)
+	while (s1[i] == s2[i] && s1[i] && s2[i] && i < (int)n - 1)
 		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (s1[i] - s2[i]);
 }
