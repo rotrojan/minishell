c05c03df3d47b0a68c870978f6621b0b488149ae
lucocozz <lucocozz@student.42.fr>
2021-05-24 16:00:43 +0200

    shitty strncmp

--- libft/string/ft_strncmp.c
+++ libft/string/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: lucocozz <lucocozz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 23:53:34 by lucocozz          #+#    #+#             */
-/*   Updated: 2021/04/29 23:53:55 by lucocozz         ###   ########.fr       */
+/*   Updated: 2021/05/24 15:59:40 by lucocozz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
-	{
-		if (s1[i] != s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	while (s1[i] == s2[i] && s1[i] && s2[i] && i < n - 1)
 		i++;
-	}
-	return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
