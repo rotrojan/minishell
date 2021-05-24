b83b4e3d5e36fd758b3db8df2a9f785d8c776ad9
lucocozz <lucocozz@student.42.fr>
2021-05-24 16:18:14 +0200

    fix ft_inenv.c

--- sources/system/env/ft_inenv.c
+++ sources/system/env/ft_inenv.c
@@ -6,7 +6,7 @@
 /*   By: lucocozz <lucocozz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 16:04:29 by lucocozz          #+#    #+#             */
-/*   Updated: 2021/05/24 15:49:42 by lucocozz         ###   ########.fr       */
+/*   Updated: 2021/05/24 16:17:45 by lucocozz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,24 @@ int	ft_inenv(const char *name)
 {
 	int		i;
 	int		len;
+	char	*var;
 	t_env	*env;
 	t_env	tmp;
 
 	i = 0;
-	len = ft_strlen(name);
+	var = ft_strjoin(name, "=", "");
+	len = ft_strlen(var);
 	env = get_shell_env();
 	tmp = *env;
 	while (tmp[i])
 	{
-		if (ft_strncmp(tmp[i], name, len) == 0)
+		if (ft_strncmp(tmp[i], var, len) == 0)
+		{
+			gc_free(var);
 			return (i);
+		}
 		i++;
 	}
+	gc_free(var);
 	return (-1);
 }
