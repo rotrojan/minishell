ba9c49836688d5f7830af9d1639bf58d3c3f2be9
Romain TROJANI-BIGOT <rotrojan@e2r5p21.clusters.42paris.fr>
2021-08-17 20:58:52 +0200

    Working on export: quote and variable expansions need to be done.

--- sources/builtins/export.c
+++ sources/builtins/export.c
@@ -6,32 +6,56 @@
 /*   By: lucocozz <lucocozz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 15:44:08 by lucocozz          #+#    #+#             */
-/*   Updated: 2021/08/15 19:15:31 by rotrojan         ###   ########.fr       */
+/*   Updated: 2021/08/17 20:55:29 by rotrojan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static bool	lex_arg(char *arg)
+{
+	unsigned int	i;
+	bool			in_squotes;
+	bool			in_dquotes;
+
+	i = 0;
+	in_squotes = FALSE;
+	in_dquotes = FALSE;
+	if (ft_isalpha(arg[i]) == FALSE)
+		return (FALSE);
+	++i;
+	while (arg[i] != NULL
+		&& (ft_isalpha(arg[i]) == TRUE || ft_isdigit(arg[i] == TRUE)))
+		++i;
+	if (!(arg[i] == '=' && (arg[i] == '+' && arg[i + 1] == '=')))
+
+}
+
+static bool	export_arg(char	*arg)
+{
+	unsigned int	i;
+	bool			in_squotes;
+
+	if (lex_arg(arg) == NULL)
+		return (FALSE);
+}
+
 int	export(int argc, char **argv)
 {
 	int		i;
-	int		j;
-	char	*var_name;
 
 	if (argc <= 1)
 		return (ft_env(argc, argv));
 	i = 1;
 	while (argv[i] != NULL)
 	{
-		j = 0;
-		while (argv[i][j] != '\0' && argv[i][j] != '=')
-			++j;
-		if (argv[i][j] != '=')
-			return (ft_env(argc, argv));
-		var_name = ft_strndup(argv[i], j);
-		ft_setenv(var_name, &argv[i][++j], 1);
-		gc_free(var_name);
+		if (export_arg(argv[i]) == FALSE)
+		{
+			ft_fprintf(STDOUT_FILENO,
+				"minishell: export: `%s': not a valid identifier\n", argv[i]);
+			return (EXIT_FAILURE);
+		}
 		++i;
 	}
-	return (0);
+	return (EXIT_SUCCESS);
 }
--- sources/expansions/expand_var.c
+++ sources/expansions/expand_var.c
@@ -6,11 +6,23 @@
 /*   By: rotrojan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/15 19:22:25 by rotrojan          #+#    #+#             */
-/*   Updated: 2021/08/16 20:34:25 by rotrojan         ###   ########.fr       */
+/*   Updated: 2021/08/17 20:56:45 by rotrojan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+char	expand_word(int *argc, char **argv)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	while ()
+	{
+
+	}
+	return (argv);
+}
 
 char	*expand_var(char const *str)
 {
@@ -30,7 +42,6 @@ char	*expand_var(char const *str)
 		{
 			
 		}
-
 	}
 	ft_get_env(var_name);
 }
