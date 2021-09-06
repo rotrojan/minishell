80ce0bdee4acedc36e1d03b95d2a455fb915e8bd
Romain TROJANI-BIGOT <rotrojan@e2r7p2.42.fr>
2021-09-06 19:23:19 +0200

    Working on export.

--- sources/builtins/export.c
+++ sources/builtins/export.c
@@ -6,36 +6,52 @@
 /*   By: lucocozz <lucocozz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 15:44:08 by lucocozz          #+#    #+#             */
-/*   Updated: 2021/09/06 16:07:08 by rotrojan         ###   ########.fr       */
+/*   Updated: 2021/09/06 19:15:27 by rotrojan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-	bool			in_squotes;
-	bool			in_dquotes;
+	/* bool			in_squotes; */
+	/* bool			in_dquotes; */
+
+	/* i = 0; */
+	/* in_squotes = FALSE; */
+	/* in_dquotes = FALSE; */
+	/* if (ft_isalpha(arg[i]) == FALSE) */
+		/* return (FALSE); */
+	/* ++i; */
+	/* while (arg[i] != '\0' */
+		/* && (ft_isalpha(arg[i]) == TRUE || ft_isdigit(arg[i] == TRUE))) */
+		/* ++i; */
+	/* [> if (!(arg[i] == '=' && (arg[i] == '+' && arg[i + 1] == '='))) <] */
+	/* return (TRUE); */
+
+/* } */
+
+static int	get_len_var_name(char *arg)
+{
+	int	i;
+	int	len_var_name;
 
 	i = 0;
-	in_squotes = FALSE;
-	in_dquotes = FALSE;
-	if (ft_isalpha(arg[i]) == FALSE)
-		return (FALSE);
-	++i;
-	while (arg[i] != '\0'
-		&& (ft_isalpha(arg[i]) == TRUE || ft_isdigit(arg[i] == TRUE)))
+	len_var_name = 0;
+	while (arg[i] != '+' && arg[i] != '=' && arg[i] != '\0')
+	{
+		if (arg[i] != '\'' || arg[i] != '"')
+			++len_var_name;
 		++i;
-	/* if (!(arg[i] == '=' && (arg[i] == '+' && arg[i + 1] == '='))) */
-	return (TRUE);
-
+	}
+	return (len_var_name);
 }
 
-static bool	get_var_name(char	*arg)
+static char	*get_var_name(char	*arg)
 {
 	int		i;
+	int		j;
 	bool	in_squotes;
 	bool	in_dquotes;
-	int		len_var_name;
-
+	char	*var_name;
 
 	i = 0;
 	in_squotes = FALSE;
@@ -52,12 +68,35 @@ static bool	get_var_name(char	*arg)
 	}
 	if (ft_isalpha(arg[i]) == 0)
 		return (NULL);
-	while (arg[i] != '+' && arg[i] != '=')
+	++i;
+	j = 0;
+	var_name = malloc(sizeof(*var_name) * (get_len_var_name(arg) + 1));
+	while (arg[i] != '+' && arg[i] != '=' && arg[i] != '\0')
 	{
-		
+		if (ft_isalnum(arg[i]) == 0)
+		{
+			if (arg[i] == '\"')
+			{
+				if (in_squotes == TRUE)
+					in_squotes = FALSE;
+				else if (in_squotes == FALSE || in_dquotes == FALSE)
+					in_squotes = TRUE;
+			}
+			else if (arg[i] == '"')
+			{
+				if (in_dquotes == TRUE)
+					in_dquotes = FALSE;
+				else if (in_dquotes == FALSE || in_squotes == FALSE)
+					in_dquotes = TRUE;
+			}
+			return (NULL);
+		}
+		else
+			var_name[j++] = arg[i];
+
 		++i;
 	}
-
+	return (var_name);
 }
 
 int	export(int argc, char **argv)
