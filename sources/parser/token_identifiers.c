4064dca406e02399080e1e562612d130322c9320
rotrojan <rotrojan@student.42.fr>
2021-10-11 20:11:03 +0200

    Parenthesis working according to minishell subject.

--- sources/parser/token_identifiers.c
+++ sources/parser/token_identifiers.c
@@ -6,7 +6,7 @@
 /*   By: lucocozz <lucocozz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 00:11:18 by rotrojan          #+#    #+#             */
-/*   Updated: 2021/09/08 20:38:28 by lucocozz         ###   ########.fr       */
+/*   Updated: 2021/10/10 16:13:39 by bigo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,4 +39,13 @@ bool	is_leaf(enum e_tok_type type)
 bool	is_parenthesis(enum e_tok_type type)
 {
 	return (type == Oparenth_tok || type == Cparenth_tok);
+}
+
+/*
+** Returns true if the token is '&&', '||' or ';'. Otherwise, returns false.
+*/
+
+bool	is_logical_operator(enum e_tok_type type)
+{
+	return (type == Semic_tok || type == Or_tok || type == And_tok);
 }
