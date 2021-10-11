1879cff29a0303cafc4af84fee16ca43c6804b38
lucocozz <lucocozz@student.42.fr>
2021-10-11 23:22:22 +0200

    input when progam is call in minishell like bash work again

--- sources/terminal/get_cursor_pos.c
+++ sources/terminal/get_cursor_pos.c
@@ -6,7 +6,7 @@
 /*   By: lucocozz <lucocozz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/06 02:53:50 by lucocozz          #+#    #+#             */
-/*   Updated: 2021/10/09 14:44:40 by lucocozz         ###   ########.fr       */
+/*   Updated: 2021/10/11 23:20:37 by lucocozz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,19 @@ static void	atoi_pos(t_axe *pos, int sep, int c)
 		pos->y = pos->y * 10 + (c - '0');
 }
 
-/* Return postion of cursor in terminal. */
+/* Return postion of cursor in terminal. 
+You must have set the terminal to non-canonical
+and without echo before. */
 t_axe	get_cursor_pos(void)
 {
 	char		c;
 	int			sep;
 	t_axe		pos;
-	t_term		*term;
 
 	c = 0;
 	sep = false;
-	ignore_signals();
-	term = set_termios();
+	signal(SIGINT, SIG_IGN);
+	signal(SIGWINCH, SIG_IGN);
 	pos = (t_axe){.x = 0, .y = 0};
 	ft_putstr_fd("\033[6n", STDIN_FILENO);
 	while (read(STDIN_FILENO, &c, 1) > 0)
@@ -46,6 +47,5 @@ t_axe	get_cursor_pos(void)
 	pos.x--;
 	pos.y--;
 	handle_signals();
-	tcsetattr(STDIN_FILENO, TCSANOW, &term->saved);
 	return (pos);
 }
